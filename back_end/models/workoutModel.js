a7a8fcd1b153ca9daeeb88bf5abf034ba027f419
PRIYA JAISWAL <jp970786@gmail.com>
2024-03-17 11:16:48 +0530

    function added

--- back_end/models/workoutModel.js
+++ back_end/models/workoutModel.js
@@ -16,6 +16,10 @@ const workoutSchema = new Schema(
       type: String,
       required: true,
     },
+    user_id: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
--- back_end/routes/workouts.js
+++ back_end/routes/workouts.js
@@ -6,20 +6,15 @@ const {
   deleteWorkout,
   updateWorkout,
 } = require("../controller/workoutController");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
+//require auth for all workout routes
+router.use(requireAuth);
 
-//GET all workouts
 router.get("/", getWorkouts);
-
-//GET a single workout
 router.get("/:id", getWorkout);
-
-//Post a new workout
 router.post("/", createWorkout);
-
 router.delete("/:id", deleteWorkout);
-
 router.patch("/:id", updateWorkout);
-
 module.exports = router;
